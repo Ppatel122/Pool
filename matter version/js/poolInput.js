319eb63f31a98945b2e809e0d42475462052dfc6
Ppatel122 <59631162+Ppatel122@users.noreply.github.com>
2021-06-24 17:35:57 -0600

    Added css

---
+++ matter version/js/poolInput.js
@@ -0,0 +1,41 @@
+var elXVel,elYVel,shootbutton,el1,el2;
+
+window.onload = () => {
+  //Prevent right-click on simulation from bringing up the context menu
+  document.oncontextmenu = function() {
+  if (mouseX > 0 && mouseY > 0 && mouseX < width && mouseY < height)
+    return false;
+  }
+  elXVel = document.querySelector("#xVel");
+  elYVel = document.querySelector("#yVel");
+  el1 = document.querySelector("#e1");
+  el2 = document.querySelector("#e2");
+  shootbutton = document.querySelector("#shootbutton");
+
+  elXVel.onchange = () => {
+    console.log("X Velocity: " + validateInput(elXVel));
+    projection.updateX(parseFloat(validateInput(elXVel)));
+    el1.innerHTML = parseFloat(elXVel.value).toFixed(3).replace('-0', '0');
+  };
+  elYVel.onchange = () => {
+    console.log("Y Velocity: " + validateInput(elYVel));
+    projection.updateY(parseFloat(validateInput(elYVel)));
+    el2.innerHTML = parseFloat(elYVel.value).toFixed(3).replace('-0', '0');
+  };
+
+  shootbutton.onclick = () => {
+    console.log("Shooting!");
+    balls[0].shoot(parseFloat(validateInput(elXVel)),parseFloat(validateInput(elYVel)));
+  }
+}
+
+function validateInput(input) {
+  if (parseFloat(input.value) > parseFloat(input.max) ) {
+    input.value = input.max;
+    return input.max;
+  } else if (parseFloat(input.value) < parseFloat(input.min) || input.value == "") {
+    input.value = input.min;
+    return input.min;
+  }
+  return input.value;
+}
